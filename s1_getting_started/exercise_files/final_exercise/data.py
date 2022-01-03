f4d1a4bcbf4af46ec36e99a6dd7bbc00f4233e12
Gustav Moesmand <gustavmoesmand@gmail.com>
2022-01-03 21:00:44 +0100

    added d1

--- s1_getting_started/exercise_files/final_exercise/data.py
+++ s1_getting_started/exercise_files/final_exercise/data.py
@@ -1,8 +1,23 @@
 import torch
+import numpy as np
+import os
 
 
-def mnist():
-    # exchange with the corrupted mnist dataset
-    train = torch.randn(50000, 784)
-    test = torch.randn(10000, 784) 
-    return train, test
+def mnist(train=True):
+    corrupt_path = "../../../data/corruptmnist"
+
+    extension = "train" if train else "test"
+    files = [np.load(os.path.join(corrupt_path, x) )
+            for x in os.listdir(corrupt_path) if extension in x]
+    
+    images = torch.from_numpy(
+        np.concatenate([x["images"] for x in files], axis=0)
+    )
+    labels = torch.from_numpy(
+        np.concatenate([x["labels"] for x in files], axis=0)
+    )
+
+    return images, labels
+
+
+
--- s1_getting_started/exercise_files/final_exercise/main.py
+++ s1_getting_started/exercise_files/final_exercise/main.py
@@ -1,7 +1,10 @@
 import argparse
 import sys
+from os import path
 
 import torch
+from torch import nn
+from torch import optim
 
 from data import mnist
 from model import MyAwesomeModel
@@ -29,15 +32,46 @@ class TrainOREvaluate(object):
     def train(self):
         print("Training day and night")
         parser = argparse.ArgumentParser(description='Training arguments')
-        parser.add_argument('--lr', default=0.1)
+        parser.add_argument('--lr', default=0.1, type=float, help="Learning Rate for optimizer")
+        parser.add_argument('--ep', default=10, type=int, help="Number of epochs")
+        parser.add_argument('--fn', default="temp", type=str, help="Filename")
         # add any additional argument that you want
         args = parser.parse_args(sys.argv[2:])
         print(args)
         
         # TODO: Implement training loop here
         model = MyAwesomeModel()
-        train_set, _ = mnist()
-        
+        train_x, train_Y = mnist(train=True)
+
+        criterion = nn.NLLLoss()
+        optimizer = optim.Adam(model.parameters(), lr=args.lr)
+
+        epochs = args.ep
+        train_losses, test_losses, test_accuracy = [], [], []
+        for e in range(epochs):
+            running_loss = 0
+            for images, labels in zip(train_x, train_Y):
+                optimizer.zero_grad()
+                images = images.resize_(images.size()[0], 784)
+                print(images.shape)
+                sys.exit()
+                output = model.forward()
+
+                loss = criterion(output, labels)
+                loss.backward()
+                optimizer.step()
+
+        checkpoint = {
+            "input_size": 784,
+            "output_size": 10,
+            "hidden_layers": [each.out_features for each in model.hidden_layers],
+            "state_dict": model.state_dict()
+        }
+
+        filename = args.fn
+        torch.save(checkpoint, path.join("model", filename + ".pth"))
+
+
     def evaluate(self):
         print("Evaluating until hitting the ceiling")
         parser = argparse.ArgumentParser(description='Training arguments')
@@ -48,7 +82,9 @@ class TrainOREvaluate(object):
         
         # TODO: Implement evaluation logic here
         model = torch.load(args.load_model_from)
-        _, test_set = mnist()
+        test_x, test_Y = mnist(train=False)
+
+    
 
 if __name__ == '__main__':
     TrainOREvaluate()
